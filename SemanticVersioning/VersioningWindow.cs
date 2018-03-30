981219fe595a78bb424534fa57a19394589f6e08
Derek Foulk <dfoulk@colheli.com>
2018-03-30 13:20:18 -0700

    Updated menu button text and window caption

--- SemanticVersioning/VersioningWindow.cs
+++ SemanticVersioning/VersioningWindow.cs
@@ -23,7 +23,7 @@
         /// </summary>
         public VersioningWindow() : base(null)
         {
-            this.Caption = "VersioningWindow";
+            this.Caption = "Semantic Versioning";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
