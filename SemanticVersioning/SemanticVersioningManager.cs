025af6490e9c139a7df3a0e82bf28d96ac781477
Derek Foulk <dfoulk@colheli.com>
2018-04-11 13:41:15 -0700

    Added Xamarin.Android & Xamarin.iOS version setting methods - These methods set the version and iterate the build number in both the 'AndroidManifest.xml' and 'Info.plist' files

--- SemanticVersioning/SemanticVersioningManager.cs
+++ SemanticVersioning/SemanticVersioningManager.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace SemanticVersioning
 {
@@ -120,44 +121,46 @@ namespace SemanticVersioning
                     catch
                     {
                     }
+
+                    try
+                    {
+                        SetMobileVersion(project, version);
+                    }
+                    catch
+                    {
+                    }
                 }
             }
         }
 
         private void SetProjectVersion(Project project, Version version)
         {
-            try
+            var projectXml = new XmlDocument();
+            projectXml.Load(project.FileName);
+
+            var versionNode = projectXml.SelectSingleNode("Project/PropertyGroup/Version");
+
+            if (versionNode == default(XmlNode))
             {
-                var projectXml = new XmlDocument();
-                projectXml.Load(project.FileName);
+                var assemblyInfoFiles = GetAssemblyInfoFiles(project);
 
-                var versionNode = projectXml.SelectSingleNode("Project/PropertyGroup/Version");
-
-                if (versionNode == default(XmlNode))
-                {
-                    var assemblyInfoFiles = GetAssemblyInfoFiles(project);
-
-                    if (assemblyInfoFiles == default(IEnumerable<string>) || !assemblyInfoFiles.Any())
-                        versionNode = projectXml.SelectSingleNode("Project/PropertyGroup").AppendChild(projectXml.CreateNode(XmlNodeType.Element, "Version", null));
-                }
-
-                versionNode.InnerText = version.ToString();
-
-                var assemblyVersionNode = projectXml.SelectSingleNode("Project/PropertyGroup/AssemblyVersion");
-
-                if (assemblyVersionNode != default(XmlNode))
-                    assemblyVersionNode.ParentNode.RemoveChild(assemblyVersionNode);
-
-                var fileVersionNode = projectXml.SelectSingleNode("Project/PropertyGroup/FileVersion");
-
-                if (fileVersionNode != default(XmlNode))
-                    fileVersionNode.ParentNode.RemoveChild(fileVersionNode);
-
-                projectXml.Save(project.FileName);
+                if (assemblyInfoFiles == default(IEnumerable<string>) || !assemblyInfoFiles.Any())
+                    versionNode = projectXml.SelectSingleNode("Project/PropertyGroup").AppendChild(projectXml.CreateNode(XmlNodeType.Element, "Version", null));
             }
-            catch
-            {
-            }
+
+            versionNode.InnerText = version.ToString();
+
+            var assemblyVersionNode = projectXml.SelectSingleNode("Project/PropertyGroup/AssemblyVersion");
+
+            if (assemblyVersionNode != default(XmlNode))
+                assemblyVersionNode.ParentNode.RemoveChild(assemblyVersionNode);
+
+            var fileVersionNode = projectXml.SelectSingleNode("Project/PropertyGroup/FileVersion");
+
+            if (fileVersionNode != default(XmlNode))
+                fileVersionNode.ParentNode.RemoveChild(fileVersionNode);
+
+            projectXml.Save(project.FileName);
         }
 
         private void SetAssemblyInfoVersion(Project project, Version version)
@@ -219,6 +222,119 @@ namespace SemanticVersioning
                 catch
                 {
                 }
+            }
+        }
+
+        private void SetMobileVersion(Project project, Version version)
+        {
+            // Check if the project is either a Xamarin.Android or a Xamarin.iOS project
+            var projectXml = new XmlDocument();
+            projectXml.Load(project.FileName);
+
+            if (projectXml == default(XmlDocument))
+                return;
+
+            var xmlNamespaceManager = new XmlNamespaceManager(projectXml.NameTable);
+            xmlNamespaceManager.AddNamespace("msbuild", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+            var projectTypeGuids = projectXml.SelectSingleNode("//msbuild:ProjectTypeGuids", xmlNamespaceManager).InnerText;
+
+            if (projectTypeGuids == null)
+                return;
+
+            var isXamarinAndroid = projectTypeGuids.Contains("{EFBA0AD7-5A72-4C68-AF49-83D382785DCF}");
+            var isXamarinIos = projectTypeGuids.Contains("{FEACFBD2-3405-455C-9665-78FE426C6842}");
+
+            if (isXamarinAndroid)
+            {
+                SetXamarinAndroidVersion(project, version);
+            }
+            else if (isXamarinIos)
+            {
+                SetXamarinIosVersion(project, version);
+            }
+        }
+
+        private void SetXamarinAndroidVersion(Project project, Version version)
+        {
+            var projectDirectory = Path.GetDirectoryName(project.FileName);
+            var androidManifestFiles = Directory.GetFiles(projectDirectory, "*AndroidManifest.xml", SearchOption.AllDirectories).Where(x => !x.Contains(@"\obj\") && !x.Contains(@"\bin\"));
+
+            foreach (string androidManifestFile in androidManifestFiles)
+            {
+                try
+                {
+                    var xDocument = XDocument.Load(androidManifestFile);
+                    XNamespace xNamespace = "http://schemas.android.com/apk/res/android";
+                    var manifest = xDocument?.Element("manifest");
+
+                    var versionCode = int.TryParse(manifest?.Attribute(xNamespace + "versionCode")?.Value, out int tmp) ? tmp : 0;
+                    versionCode++;
+
+                    manifest?.SetAttributeValue(xNamespace + "versionCode", versionCode.ToString());
+                    manifest?.SetAttributeValue(xNamespace + "versionName", version.ToString());
+
+                    xDocument.Save(androidManifestFile);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void SetXamarinIosVersion(Project project, Version version)
+        {
+            var projectDirectory = Path.GetDirectoryName(project.FileName);
+            var infoFiles = Directory.GetFiles(projectDirectory, "*Info.plist", SearchOption.AllDirectories).Where(x => !x.Contains(@"\obj\") && !x.Contains(@"\bin\"));
+
+            foreach (string infoFile in infoFiles)
+            {
+                try
+                {
+                    var xDocument = XDocument.Load(infoFile);
+
+                    xDocument.DocumentType.InternalSubset = null;
+
+                    var dict = xDocument?.Element("plist")?.Element("dict");
+
+                    var bundleVersion = decimal.TryParse((dict?.Descendants("key")?.FirstOrDefault(x => x.Value == "CFBundleVersion")?.NextNode as XElement)?.Value, out decimal tmp) ? (int)tmp : 0;
+                    bundleVersion++;
+
+                    PlistAddOrUpdate("CFBundleVersion", bundleVersion.ToString(), dict);
+
+                    PlistAddOrUpdate("CFBundleShortVersionString", version.ToString(), dict);
+
+                    xDocument.Save(infoFile);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void PlistAddOrUpdate(string key, string value, XElement dict)
+        {
+            var currentKey = dict?.Descendants("key")?.FirstOrDefault(x => x.Value == key);
+
+            if (currentKey == default(XElement))
+            {
+                var keyNode = new XElement("key")
+                {
+                    Value = key
+                };
+
+                var valueNode = new XElement("string")
+                {
+                    Value = value
+                };
+
+                dict?.Add(keyNode);
+                dict?.Add(valueNode);
+            }
+            else
+            {
+                if (currentKey?.NextNode is XElement currentValue && currentValue.Name == "string")
+                    currentValue.Value = value;
             }
         }
 
