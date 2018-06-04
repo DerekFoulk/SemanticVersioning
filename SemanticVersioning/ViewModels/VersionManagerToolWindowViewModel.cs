74037eb474a74643ae1148b9068a7eff985f46ef
Derek Foulk <dfoulk@colheli.com>
2018-06-04 10:37:32 -0700

    Updated view and model - Added data binding

--- SemanticVersioning/ViewModels/VersionManagerToolWindowViewModel.cs
+++ SemanticVersioning/ViewModels/VersionManagerToolWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace SemanticVersioning.ViewModels
+﻿using SemanticVersioning.Models;
+
+namespace SemanticVersioning.ViewModels
 {
-    class VersionManagerToolWindowViewModel
+    public class VersionManagerToolWindowViewModel
     {
+        public VersionManagerToolWindowViewModel()
+        {
+            Version = new Version("1.0.0");
+        }
+
+        public Version Version { get; set; }
     }
 }
--- SemanticVersioning/Views/VersionManagerToolWindowControl.xaml.cs
+++ SemanticVersioning/Views/VersionManagerToolWindowControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace SemanticVersioning.Views
 {
+    using SemanticVersioning.ViewModels;
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
@@ -15,20 +16,8 @@
         public VersionManagerToolWindowControl()
         {
             this.InitializeComponent();
-        }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "VersionManagerToolWindow");
+            DataContext = new VersionManagerToolWindowViewModel();
         }
     }
 }
